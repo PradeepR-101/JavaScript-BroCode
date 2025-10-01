6e9cec02e7307c8c3293acadb606b76c696c3886
PradeepR-101 <78174048+PradeepR-101@users.noreply.github.com>
2025-10-01 21:39:51 +0530

    Update index.js

--- 36 spread operator/index.js
+++ 36 spread operator/index.js
@@ -21,3 +21,24 @@ let class2 = ["Squidward", "Eugene", "Plankton"];
 class1.push(...class2);
 
 console.log(...class1)
+
+
+// ------------ EXAMPLE 1 ------------
+let numbers = [1, 2, 3, 4, 5];
+let maximum = Math.max(...numbers);  //5
+let minimum = Math.min(...numbers);  //1
+
+console.log(maximum);
+
+// ------------ EXAMPLE 2 ------------
+let username = "Bro Code";
+let letters = [...username];  
+
+console.log(letters);  //["B","r","o"," ","C","o","d","e"]
+
+// ------------ EXAMPLE 3 ------------
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrots", "celery", "potatoes"];
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
