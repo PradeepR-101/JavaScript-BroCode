105b30482cbb312ed4de7b368167cac4b09fc875
PradeepR-101 <78174048+PradeepR-101@users.noreply.github.com>
2025-10-01 21:56:19 +0530

    Update index.js

--- 02 variables/index.js
+++ 02 variables/index.js
@@ -18,3 +18,21 @@ console.log("Enrolled:", student);
 document.getElementById("p1").innerHTML = "Hello " + firstName;
 document.getElementById("p2").innerHTML = "You are " + age + " years old!";
 document.getElementById("p3").innerHTML = "Enrolled: " + firstName;
+
+============================================
+// variable scope = where a variable is recognized
+// and accessible (local vs global)            
+
+let x = 3; // global scope
+
+function1();
+
+function function1(){
+    let x = 1; // local scope
+    console.log(x);
+}
+
+function function2(){
+    let x = 2; // local scope
+    console.log(x);
+}
