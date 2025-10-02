a8cc758492013e4489ae0b7d1de5ed1eb0684edb
PradeepR-101 <78174048+PradeepR-101@users.noreply.github.com>
2025-10-02 15:46:06 +0530

    Create 47_ThisKeyword.js

---
+++ 47_ThisKeyword.js
@@ -0,0 +1,19 @@
+// this = reference to the object where THIS is used
+// (the object depends on the immediate context)
+// this keyword will not work for arrow function use instead use object reference name itself
+
+const person1 = {
+    name: "Spongebob",
+    favFood: "hamburgers",
+    sayHello: function(){console.log(`Hi! I am ${this.favFood}`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+}
+const person2 = {
+    name: "Patrick",
+    favFood: "pizza",
+    sayHello: function(){console.log(`Hi! I am ${this.favFood}`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
+}
+
+person1.eat();
+person2.eat();
